e5c9297a73d9e8558654df114942212ed5f65557
github username <theoluyi@gmail.com>
2021-03-10 20:58:09 -0500

    working solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    const obj = {};
+    for (const char of str) {
+        obj[char] = obj[char]? obj[char] + 1 : 1;
+    }
+    
+    // {null: 0} // dont want POJO bc keys harder to overwrite than array elements
+    const highFreq =  [null, 0] 
+    for (const key in obj) {
+        const thisFreq = obj[key];
+        if (thisFreq > highFreq[1]) {
+            highFreq[1] = thisFreq;
+            highFreq[0] = key;
+        }
+    }
+
+    debugger;
+
+    return highFreq[0]
+}
+
+maxChar("abcccccccd")
 
 module.exports = maxChar;
