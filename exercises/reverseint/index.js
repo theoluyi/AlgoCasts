4470fb98df75d861ca55b6d502188325ee5a0ed1
github username <theoluyi@gmail.com>
2021-03-10 19:43:04 -0500

    decent solution to reverseint. why is there no appended toNumber() method for js strings besides * 1? annoying I cant continue chaining

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if (n.toString()[0] !== '-') {
+        return n.toString().split("").reverse().join("") * 1;
+    }
+    if (n.toString()[0] === '-') {
+        const stringNum = n.toString().split("").reverse()
+        stringNum.unshift(stringNum.pop())
+        return stringNum.join("") * 1
+    }
+
+}
 
 module.exports = reverseInt;
