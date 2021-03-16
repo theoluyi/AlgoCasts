f49da0b2bb0fdff07607d627204fa940f13f0e5f
github username <theoluyi@gmail.com>
2021-03-16 11:31:05 -0400

    standard solution, with thoughts on if/if else/else statements being right-side up decision trees

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -14,13 +14,46 @@
 
 function fizzBuzz(n) {
     for (let i = 1; i <= n; i++) {
-        let message = i;
-        if (i % 3 === 0) {message = "fizz"}
-        if (i % 5 === 0) {message = "buzz"}
-        if (i % 3 === 0 && i % 5 === 0) {message = "fizzbuzz"}
-        console.log(message)
-    }
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz")
+        } else if (i % 3 === 0) { 
+            console.log("fizz")
+        } else if (i % 5 === 0) { 
+            console.log("buzz")
+        }    
+        else {
+            console.log(i)
+        }
+    } // end for loop
 }
+
+// is this logic like working upward from the bottom
+// of a choice tree?
+// the else condition is the most general
+// i % 15 === 0 is the most specific
+// i % 3 === 0 and i % 5 === 0 are less specific than
+// i % 15 === 0, but as specific as each other 
+// so the logic (if ... if else ... else) statement flow
+// looks like an upside down tree, with the roots at the top (the most)
+// or it could be right side up if we just think about a typical data tree
+// as being already inverted
+
+// yes, that is it: by convention, trees are drawn growing downwards
+// The topmost node in a tree is called the root node. 
+
+// so with that in mind, this is a right side up tree, drawn against the convention
+// so that it looks like a real life tree, where the most superficial
+// aspects are at the top, and the most generalized is at the bottom (root)
+
+// function fizzBuzz(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let message = i;
+//         if (i % 3 === 0) {message = "fizz"}
+//         if (i % 5 === 0) {message = "buzz"}
+//         if (i % 3 === 0 && i % 5 === 0) {message = "fizzbuzz"}
+//         console.log(message)
+//     }
+// }
 
 // second solution: uses assignment
 // function fizzBuzz(n) {
