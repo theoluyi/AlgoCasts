7ef3e207a1de93829db241e5a21787643010dc77
github username <theoluyi@gmail.com>
2021-03-17 13:08:40 -0400

    minor changes

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -11,7 +11,6 @@
 /**
  * Questions
  * A) are we mutating the original array?
- * 
  */
 
 /**
@@ -21,6 +20,8 @@
  * but just there's pushing happening inside the for loop
  * 
  * having trouble thinking about initializing each chunk, where to do it???
+ * ahh, so we worked this out by having it be initialized outside the for loop
+ * and pointing it to a new array in memory at the end of every for loop iteration
  */
 
 function chunk(array, size) {
@@ -49,5 +50,33 @@ function chunk(array, size) {
     // don't forget to return something!
     return newArray;
 }
+
+// first working solution!!! No help from the Grider!!!
+// function chunk(array, size) {
+//     // initialize the return value, a newArray full of chunks
+//     let newArray = []
+//     // initialize the chunk, which will be added to the new array
+//     let chunk = []
+
+//     // loop over the original array
+//     for (const element of array) {
+//         // if the chunk is still smaller than its max size, push another element into it
+//         // I think this might fail because if we run out of elements we aren't adding the new chunk
+//         if (chunk.length < size) {
+//             chunk.push(element)
+//         } 
+//         if (chunk.length === size || array[array.length-1] === element) {
+//         // if the chunk is large enough, or we are at the last available element (QQ imprecise condition)
+//         // note: condition is imprecise because we are checking the element value, not its index
+//         // if we used a traditional for loop instead, there'd definitely be a certain check
+//         // add the chunk to the newArray
+//             newArray.push(chunk)
+//         // repoint the chunk variable to a different array in memory, so we can build a new chunk
+//             chunk = []
+//         }
+//     }
+//     // don't forget to return something!
+//     return newArray;
+// }
 
 module.exports = chunk;
