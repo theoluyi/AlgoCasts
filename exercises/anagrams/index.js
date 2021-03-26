e58e3c318d0f8774aa5b21f1bd47842828ab8dff
github username <theoluyi@gmail.com>
2021-03-25 23:24:49 -0400

    minor improvements

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,43 +9,70 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    const objectA = {};
-    const objectB = {};
-
     // Check if strings are the same length, if not they can't be anagrams
     if (stringA.length !== stringB.length) {
         return false;
     }
+
+    const objectA = {};
+    const objectB = {};
+
     const simpStringA = stringA.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase()
     const simpStringB = stringB.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase()
-    // console.log('simpStringA :>> ', simpStringA);
-    // console.log('simpStringB :>> ', simpStringB);
+
     for (const char of simpStringA) {
-        if (objectA[char]) {
-            objectA[char] += 1;
-        }
-        else {
-            objectA[char] = 1;
-        }
+        objectA[char] = objectA[char] + 1 || 1;
     }
     for (const char of simpStringB) {
-        if (objectB[char]) {
-            objectB[char] += 1;
-        }
-        else {
-            objectB[char] = 1;
-        }
+        objectB[char] = objectB[char] + 1 || 1;
     }
 
     // We want to check to make sure that these objects have the same number of keys
     for (const key in objectA) {
         if (objectA[key] !== objectB[key]) {
-            return false
+            return false;
         }
-
     }
     return true;
-
 }
 
+// First working solution
+// function anagrams(stringA, stringB) {
+//     const objectA = {};
+//     const objectB = {};
+
+//     // Check if strings are the same length, if not they can't be anagrams
+//     if (stringA.length !== stringB.length) {
+//         return false;
+//     }
+//     const simpStringA = stringA.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase()
+//     const simpStringB = stringB.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase()
+//     // console.log('simpStringA :>> ', simpStringA);
+//     // console.log('simpStringB :>> ', simpStringB);
+//     for (const char of simpStringA) {
+//         if (objectA[char]) {
+//             objectA[char] += 1;
+//         }
+//         else {
+//             objectA[char] = 1;
+//         }
+//     }
+//     for (const char of simpStringB) {
+//         if (objectB[char]) {
+//             objectB[char] += 1;
+//         }
+//         else {
+//             objectB[char] = 1;
+//         }
+//     }
+
+//     // We want to check to make sure that these objects have the same number of keys
+//     for (const key in objectA) {
+//         if (objectA[key] !== objectB[key]) {
+//             return false;
+//         }
+
+//     }
+//     return true;
+// }
 module.exports = anagrams;
